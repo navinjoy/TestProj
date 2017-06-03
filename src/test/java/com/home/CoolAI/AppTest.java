240919f155631b811725022ea1d35bc7a3a34297
Deepika J <deepikajammula4@gmail.com>
2017-06-03 15:59:40 -0700

    testng suite updated

--- src/test/java/com/home/CoolAI/AppTest.java
+++ src/test/java/com/home/CoolAI/AppTest.java
@@ -1,38 +1,23 @@
 package com.home.CoolAI;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.testng.Assert.assertTrue;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
+import org.testng.annotations.Test;
+
+public class AppTest
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	@Test
+	public void  Test009 ()
+	{
+		System.out.println("gghjgjgj");;
+	}
+
+
+	@Test
+	public void testApp()
+	{
+		assertTrue( true );
+	}
 }
