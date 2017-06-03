90742927740fc4a65c69dcb49c792ddc14e9f0a0
Deepika J <deepikajammula4@gmail.com>
2017-06-03 16:46:55 -0700

    added suite file

--- src/test/java/com/home/CoolAI/AppTest.java
+++ src/test/java/com/home/CoolAI/AppTest.java
@@ -11,13 +11,26 @@ public class AppTest
 	@Test
 	public void  Test009 ()
 	{
-		System.out.println("gghjgjgj");;
+		System.out.println("Test 1");;
 	}
 
+	@Test
+	public void  Test001 ()
+	{
+		System.out.println("Test 2");
+	}
 
 	@Test
 	public void testApp()
 	{
 		assertTrue( true );
+		System.out.println("Test 3");
+
+	}
+	
+	@Test
+	public void testRuntimearg() {
+		String runTimeArg = System.getProperty("runtimearg");
+		System.out.println(runTimeArg);
 	}
 }
